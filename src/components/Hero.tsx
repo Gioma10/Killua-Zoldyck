5d2f1344469c07a639d06c8be3e0ca614480c739
Giovanni Mauro <giovi.mauro03@gmail.com>
2025-09-22 18:30:26 +0200

    header in progress

---
+++ src/components/Hero.tsx
@@ -0,0 +1,19 @@
+const Hero = () => {
+  return (
+    <div
+      className="hero min-h-screen"
+      style={{
+        backgroundImage:
+          "url(https://images.unsplash.com/photo-1654198340681-a2e0fc449f1b?q=80&w=2070&auto=format&fit=crop&ilib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D)",
+      }}
+    >
+      <div className="hero-overlay"></div>
+      <div className="hero-content text-neutral-content text-center">
+        <div className="max-w-md">
+          <h1 className="mb-5 text-5xl font-bold">Killua Zoldyck</h1>
+        </div>
+      </div>
+    </div>
+  );
+};
+export default Hero;
